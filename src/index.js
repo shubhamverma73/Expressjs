24261340dba26987a348492ff7c523502833304e
Shubham <shubhamkrverma73@gmail.com>
2021-03-25 18:29:48 +0530

    use HBS for call template engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 var app = express();
-const path = require('path');
+//const path = require('path');
 
-const staticPath = path.join(__dirname, '../public');
-app.use(express.static(staticPath));
+/* using HBS template enging  */
+app.set('view engine', 'hbs');
+app.get('/', (req, res) => {
+    res.render('index', {
+        totalExp: '8+ Years'
+    });
+});
+
+/* using normal html page call  */
+//const staticPath = path.join(__dirname, '../public');
+//app.use(express.static(staticPath));
 
 app.listen(5000, () => {
     console.log('listeing the port 5000');
