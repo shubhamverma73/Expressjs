36de1dc43da9e2ead85ce2ca7966f30e6aead17d
Shubham <shubhamkrverma73@gmail.com>
2021-03-31 14:55:06 +0530

    some file schanges

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 var app = express();
-//const path = require('path');
+const path = require('path');
+const hbs = require('hbs');
 
 /* using HBS template enging  */
+const templatePath = path.join(__dirname, './templates/views');
+const partialsPath = path.join(__dirname, './templates/partials');
+
 app.set('view engine', 'hbs');
+app.set('views', templatePath);
+hbs.registerPartials(partialsPath);
+
 app.get('/', (req, res) => {
     res.render('index', {
         totalExp: '8+ Years'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        bodyPart: 'The content of the About......'
     });
 });
 
---
+++ src/templates/partials/scripts.hbs
@@ -0,0 +1,10 @@
+<script>
+// Script to open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+</script>
