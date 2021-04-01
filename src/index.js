24012f1dc771d7201535289516b9cda1aba46781
Shubham <shubhamkrverma73@gmail.com>
2021-04-01 18:33:55 +0530

    weather application changes

--- src/index.js
+++ src/index.js
@@ -25,31 +25,19 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-    /*res.render('weather', {
-        bodyPart: 'The content of the About......'
-    });*/
     var city = req.query.name == '/' ? 'New Delhi' : req.query.name.replace('/', '');        
     requests(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=725ae9776c04b5f5511236fb804c623a&units=metric`)
     .on('data', (response) => {
         const jsonResponse = JSON.parse(response);
         const arrData = [jsonResponse];
-        //console.log(`${arrData[0].name} ${arrData[0].main.temp}`);
-        var realTimeData = '';
-        /*if(arrData[0].cod != 404) {
-            var realTimeData = arrData;
-        } else {
-            var realTimeData = `<!DOCTYPE html><html><body>The city you entered ${city.bold()} is not found in our records.</body></html>`;
-        }*/
         res.render('weather', {
-            bodyPart: realTimeData
+            bodyPart: arrData[0].name
         });
-        //res.write(realTimeData);
-        //res.write(`City name is ${arrData[0].name} and the current tempreatur is ${arrData[0].main.temp}`);
     })
-    .on('end', (err) => {
+    /*.on('end', (err) => {
         if (err) return console.log('connection closed due to errors', err);        
         res.end();
-    });
+    });*/
 });
 
 app.get('*', (req, res) => {
